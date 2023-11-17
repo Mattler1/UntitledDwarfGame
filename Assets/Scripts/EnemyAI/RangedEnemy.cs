68f936c123ca9bfef6cf903f8c1a539549e12965
Matthew Davis <littlebuilder1@gmail.com>
2023-11-17 15:50:30 -0700

    Working on Ranged AI

--- Assets/Scripts/EnemyAI/RangedEnemy.cs
+++ Assets/Scripts/EnemyAI/RangedEnemy.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class RangedEnemy : MonoBehaviour
@@ -7,34 +8,87 @@ public class RangedEnemy : MonoBehaviour
     public float attackRange;
     private readonly Transform playerTransform = GameObject.Find("PlayerPrefab").GetComponent<Transform>();
     private List<Vector3> destinations = new();
+    private readonly int timeBetweenShots = 5;
+    private bool isRunningAway = false;
+    private bool bulletReady = false;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        StartCoroutine(PrepareBullet());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Physics.Raycast(this.GetComponent<Transform>().position, (playerTransform.position - this.GetComponent<Transform>().position).normalized, out RaycastHit hit, attackRange))
+        if (Physics.Raycast(transform.position, (playerTransform.position - transform.position).normalized, out RaycastHit hit, attackRange))
         {
             if (hit.transform.gameObject.CompareTag("Player"))
             {
-                
+                // I need a model for our chosen projectile.
+                FireAtPlayer();
             }
         }
-
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        if (!agent.pathPending && agent.remainingDistance <= 0.5f)
         {
-            SetNewDestination();
+            if (destinations.Count <= 0)
+            {
+                SetNewDestinations();
+                ChooseNewDestination();
+            }
+            else
+            {
+                ChooseNewDestination();
+            }
         }
     }
 
-    private void SetNewDestination()
+    private void SetNewDestinations()
     {
-        destinations.Add(this.GetComponent<Transform>().position - new Vector3(Random.Range(1f, 3f), 0f, Random.Range(1f, 3f)));
-        destinations.Add(this.GetComponent<Transform>().position - new Vector3(Random.Range(1f, 3f), 0f, Random.Range(1f, 3f)));
-        destinations.Add(this.GetComponent<Transform>().position + new Vector3(Random.Range(1f, 3f), 0f, Random.Range(1f, 3f)));
-        destinations.Add(this.GetComponent<Transform>().position + new Vector3(Random.Range(1f, 3f), 0f, Random.Range(1f, 3f)));
+        
+        if (Vector3.Distance(transform.position, playerTransform.position) <= 10f)
+        {
+            destinations.Add(transform.position - ((Quaternion.AngleAxis(Random.Range(0, 179), Vector3.up) * (playerTransform.position - transform.position).normalized)) * 10f);
+        }
+        else
+        {
+            destinations.Add(Vector3.Reflect(transform.position, (Vector3.left * 2f)));
+            destinations.Add(Vector3.Reflect(transform.position, (Vector3.right * 2f)));
+        }
+    }
+
+    private void ChooseNewDestination()
+    {
+        Vector3 chosenDestination = destinations[0];
+        agent.destination = chosenDestination;
+        destinations.Remove(chosenDestination);
+    }
+
+    private void FireAtPlayer()
+    {
+        Vector3 predictedPosition = playerTransform.position;
+        float velocity = playerTransform.GetComponent<Rigidbody>().velocity;
+
+        velocity *= Time.deltaTime;
+        predictedPosition += velocity;
+
+        if (bulletReady)
+        {
+            //Wait for a projectile object to be available to fire before finishing this.
+            return;
+        }
+    }
+
+    private IEnumerator PrepareBullet()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            if (!bulletReady)
+            {
+                yield return new WaitForSecondsRealtime(5f);
+                bulletReady = true;
+            }
+        }
     }
 }
