193590261d165379d6f2ea8961fb02073a19c994
Matthew Davis <littlebuilder1@gmail.com>
2023-12-04 14:57:17 -0700

    Adding scalability

--- Assets/Scripts/InstantiateScripts/InstantiateEnemy.cs
+++ Assets/Scripts/InstantiateScripts/InstantiateEnemy.cs
@@ -6,11 +6,22 @@ public class InstantiateEnemy : MonoBehaviour
 {
     public List<GameObject> enemiesToSpawn;
     public List<Transform> enemySpawnPositions;
+    public Rigidbody triggerObject;
     private Rigidbody playerBody;
 
     // Start is called before the first frame update
     void Start()
     {
         playerBody = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        RaycastHit[] hits = playerBody.SweepTestAll(playerBody.transform.forward, playerBody.velocity, QueryTriggerInteraction.Collide);
+
+        for (int i = 0; i < hits.length; i++)
+        {
+            break;
+        }
     }
 }
