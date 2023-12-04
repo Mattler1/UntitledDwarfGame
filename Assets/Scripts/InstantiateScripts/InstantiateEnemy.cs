f42f4968d6b378cb2075acab3e610bae95c6add8
Matthew Davis <littlebuilder1@gmail.com>
2023-12-04 09:31:01 -0700

    Working on scalability and room spawning

--- Assets/Scripts/InstantiateScripts/InstantiateEnemy.cs
+++ Assets/Scripts/InstantiateScripts/InstantiateEnemy.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class InstantiateEnemy : MonoBehaviour
 {
-    public GameObject enemyPrefab;
-    public Transform enemySpawnPosition;
+    public List<GameObject> enemiesToSpawn;
+    public List<Transform> enemySpawnPositions;
+    private Rigidbody playerBody;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, enemySpawnPosition.position, Quaternion.identity);
+        playerBody = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
     }
 }
